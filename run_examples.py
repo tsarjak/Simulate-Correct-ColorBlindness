b19b4f8edb3fa53c618e70f1d12bb1169cbe7e69
Sarjak Thakkar <tsarjak@gmail.com>
2021-04-02 17:10:45 -0400

    Update the example for readme

--- run_examples.py
+++ run_examples.py
@@ -3,55 +3,69 @@ from recolor import Core
 
 def main():
     # Simulating Protanopia with diagnosed degree of 0.9 and saving the image to file.
-    Core.simulate(input_path='Examples_Check/example_original.jpg',
+    Core.simulate(input_path='Examples_Check/ex_original.jpg',
                   return_type='save',
-                  save_path='Examples_Check/example_simulate_protanopia.png',
+                  save_path='Examples_Check/ex_simulate_protanopia.png',
                   simulate_type='protanopia',
                   simulate_degree_primary=0.9)
 
     # Simulating Deutranopia with diagnosed degree of 0.9 and saving the image to file.
-    Core.simulate(input_path='Examples_Check/example_original.jpg',
+    Core.simulate(input_path='Examples_Check/ex_original.jpg',
                   return_type='save',
-                  save_path='Examples_Check/example_simulate_deutranopia.png',
+                  save_path='Examples_Check/ex_simulate_deutranopia.png',
                   simulate_type='deutranopia',
                   simulate_degree_primary=0.9)
 
     # Simulating Tritanopia with diagnosed degree of 0.9 and saving the image to file.
-    Core.simulate(input_path='Examples_Check/example_original.jpg',
+    Core.simulate(input_path='Examples_Check/ex_original.jpg',
                   return_type='save',
-                  save_path='Examples_Check/example_simulate_tritanopia.png',
+                  save_path='Examples_Check/ex_simulate_tritanopia.png',
                   simulate_type='tritanopia',
                   simulate_degree_primary=0.9)
 
     # Simulating Hybrid (Protanomaly + Deutranomaly) with diagnosed degree of 0.9 and 1.0 and saving the image to file.
-    Core.simulate(input_path='Examples_Check/example_original.jpg',
+    Core.simulate(input_path='Examples_Check/ex_original.jpg',
                   return_type='save',
-                  save_path='Examples_Check/example_simulate_deutranopia.png',
-                  simulate_type='deutranopia',
-                  simulate_degree_primary=0.9,
-                  simulate_degree_sec=1.0)
+                  save_path='Examples_Check/ex_simulate_hybrid.png',
+                  simulate_type='hybrid',
+                  simulate_degree_primary=0.5,
+                  simulate_degree_sec=0.5)
 
     # Correcting Image for Protanopia with diagnosed degree of 1.0 and saving the image to file.
-    Core.correct(input_path='Examples_Check/example_original.jpg',
+    Core.correct(input_path='Examples_Check/ex_original.jpg',
                  return_type='save',
-                 save_path='Examples_Check/example_corrected_protanopia.png',
-                 protanopia_degree=1.0,
+                 save_path='Examples_Check/ex_corrected_protanopia.png',
+                 protanopia_degree=0.9,
                  deutranopia_degree=0.0)
 
+    # Also simulate the corrected image to see difference.
+    Core.simulate(input_path='Examples_Check/ex_corrected_protanopia.png',
+                  return_type='save',
+                  save_path='Examples_Check/ex_simulate_corrected_protanopia.png',
+                  simulate_type='protanopia',
+                  simulate_degree_primary=0.9)
+
     # Correcting Image for Deutranopia with diagnosed degree of 1.0 and saving the image to file.
-    Core.correct(input_path='Examples_Check/example_original.jpg',
+    Core.correct(input_path='Examples_Check/ex_original.jpg',
                  return_type='save',
-                 save_path='Examples_Check/example_corrected_deutranopia.png',
+                 save_path='Examples_Check/ex_corrected_deutranopia.png',
                  protanopia_degree=0.0,
                  deutranopia_degree=1.0)
 
+    # Also simulate the corrected image to see difference.
+    Core.simulate(input_path='Examples_Check/ex_corrected_deutranopia.png',
+                  return_type='save',
+                  save_path='Examples_Check/ex_simulate_corrected_deutranopia.png',
+                  simulate_type='deutranopia',
+                  simulate_degree_primary=0.9)
+
     # Correcting Image for Hybrid with diagnosed degree of 1.0 for both protanopia and
     # deutranopia and saving the image to file.
-    Core.correct(input_path='Examples_Check/example_original.jpg',
+    Core.correct(input_path='Examples_Check/ex_original.jpg',
                  return_type='save',
-                 save_path='Examples_Check/example_corrected_hybrid.png',
-                 protanopia_degree=1.0,
-                 deutranopia_degree=1.0)
+                 save_path='Examples_Check/ex_corrected_hybrid.png',
+                 protanopia_degree=0.5,
+                 deutranopia_degree=0.5)
 
     # You can also use different return types and get numpy array or PIL.Image for further processing.
     # See recolor.py
